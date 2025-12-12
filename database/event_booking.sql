8993b25329892a77eb180d1f9fbde0a450a5a1a0
RohanMalakar <rohanmalakar5091@gmail.com>
2025-12-13 01:56:35 +0530

    change the role

--- database/event_booking.sql
+++ database/event_booking.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('user', 'admin') DEFAULT 'user',
+    role ENUM('user', 'admin') DEFAULT 'admin',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
